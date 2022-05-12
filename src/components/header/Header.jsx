4922181804454da472da8eb5aca1355c2bbf63ea
James McNeil <mcneil_james@hotmail.com>
2022-05-12 21:42:17 +0100

    searchBar: ready to work on incr options

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -13,12 +13,20 @@ import {
 } from '@fortawesome/free-solid-svg-icons'
 import { faCalendarDays } from '@fortawesome/free-regular-svg-icons'
 import { DateRange } from 'react-date-range'
+import { format } from 'date-fns'
 
 const Header = () => {
+  const [openDate, setOpenDate] = useState(false)
+  const [openOptions, setOpenOptions] = useState(false)
+  const [options, setOptions] = useState({
+    adult: 1,
+    children: 0,
+    rooms: 1,
+  })
   const [date, setDate] = useState([
     {
       startDate: new Date(),
-      endDate: null,
+      endDate: new Date(),
       key: 'selection',
     },
   ])
@@ -66,18 +74,53 @@ const Header = () => {
           </div>
           <div className='headerSearchItem'>
             <FontAwesomeIcon icon={faCalendarDays} className='headerIcon' />
-            <span className='headerSearchText'>date to date</span>
-            <DateRange
-              editableDateInputs={true}
-              onChange={(item) => setDate([item.selection])}
-              moveRangeOnFirstSelection={false}
-              ranges={date}
-              className='date'
-            />
+            <span
+              className='headerSearchText'
+              onClick={() => {
+                setOpenDate(!openDate)
+              }}>{`${format(date[0].startDate, 'MM/dd/yyyy')} to ${format(
+              date[0].endDate,
+              'MM/dd/yyyy'
+            )}`}</span>
+            {openDate && (
+              <DateRange
+                editableDateInputs={true}
+                onChange={(item) => setDate([item.selection])}
+                moveRangeOnFirstSelection={false}
+                ranges={date}
+                className='date'
+              />
+            )}
           </div>
           <div className='headerSearchItem'>
             <FontAwesomeIcon icon={faPerson} className='headerIcon' />
-            <span className='headerSearchText'>2 adults 2 children 1 room</span>
+            <span className='headerSearchText'>{`${options.adult} adults · ${options.children} children · ${options.rooms} room`}</span>
+            <div className='options'>
+              <div className='optionItem'>
+                <span className='optionText'>Adult</span>
+                <div className='optionCounter'>
+                  <button className='optionCounterButton'>-</button>
+                  <span className='optionCounterNumber'>1</span>
+                  <button className='optionCounterButton'>+</button>
+                </div>
+              </div>
+              <div className='optionItem'>
+                <span className='optionText'>Children</span>
+                <div className='optionCounter'>
+                  <button className='optionCounterButton'>-</button>
+                  <span className='optionCounterNumber'>0</span>
+                  <button className='optionCounterButton'>+</button>
+                </div>
+              </div>
+              <div className='optionItem'>
+                <span className='optionText'>Room</span>
+                <div className='optionCounter'>
+                  <button className='optionCounterButton'>-</button>
+                  <span className='optionCounterNumber'>1</span>
+                  <button className='optionCounterButton'>+</button>
+                </div>
+              </div>
+            </div>
           </div>
           <div className='headerSearchItem'>
             <button className='headerBtn'>Search</button>
